f64c469fcf603d603307a38db79bf50a36832046
fannix6 <b.fannisuli@gmail.com>
2025-11-25 12:02:45 +0100

    StudentFactory.php

--- server/database/factories/StudentFactory.php
+++ server/database/factories/StudentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Schoolclass;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +10,80 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class StudentFactory extends Factory
 {
+    protected function withFaker()
+    {
+        // Manuális beállítás az app config felülírására
+        return \Faker\Factory::create('hu_HU');
+    }
+
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
+
+    function kalkulalOsztöndijTömbbel(float $atlag): int
+    {
+        $osztondijTabla = [
+            2.0 => 8000,
+            2.5 => 16000,
+            3.5 => 25000,
+            4.5 => 42000,
+            // Az 59000-et a ciklus utáni alapértelmezett érték garantálja.
+        ];
+ 
+        foreach ($osztondijTabla as $hatar => $osszeg) {
+            if ($atlag < $hatar) {
+                return $osszeg;
+            }
+        }
+        return 59000;
+    }
+
     public function definition(): array
     {
+        //neme
+        $neme = $this->faker->boolean;
+        $gender = $neme ? 'male' : 'female';
+
+        //neve
+        $firstName = $this->faker->firstName($gender);
+        $lastName = $this->faker->lastName($gender);
+        $diakNev = "$firstName $lastName";
+
+        //adatok
+        $iranyitoszam = $this->faker->postcode;
+        $lakHelyseg = $this->faker->city;
+        $szulHelyseg = $this->faker->city;
+        $lakCim = $this->faker->streetAddress;
+        $igazolvanyszam = strtoupper($this->faker->bothify('??######'));
+        
+        //osztaly
+        $atlag = $this->faker->randomFloat($nbMaxDecimals = 1, $min = 1, $max = 5);
+        $randomClass = Schoolclass::inRandomOrder()->first();
+        $schoolclassId = $randomClass->id;
+        $osztondij = 20000;
+
+        
+        $grade =(int) substr($randomClass->osztalyNev, 0, 1);
+        $ageMin = $grade+5;
+        $ageMax = $grade+6;
+        $szulDatum = $this->faker->dateTimeBetween(($ageMax). ' years', ($ageMin). ' years');
+        
         return [
-            //
+            'diakNev' => $diakNev,
+            'schoolclassId' => $schoolclassId,
+            'neme' => $neme,
+            'iranyitoszam' => $iranyitoszam,
+            'lakHelyseg' => $lakHelyseg,
+            'lakCim' => $lakCim,
+            'szulHelyseg' => $szulHelyseg,
+            'szulDatum' => $szulDatum,
+            'igazolvanyszam' => $igazolvanyszam,
+            'atlag' => $atlag,
+            'osztondij' => $osztondij,
+
+
         ];
     }
 }
--- server/database/seeders/StudentSeeder.php
+++ server/database/seeders/StudentSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Schoolclass;
+use App\Models\Student;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,12 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //egy osztalyba atleg 28 tanulo jar
+        $avgClassSize = 28;
+        //osszesen hany osztalyunk van
+        $numberOfClasses = Schoolclass::count();
+        //akkor hany tanulonk legyen
+        $numberOfStudent = $avgClassSize * $numberOfClasses;
+        Student::factory()->count($numberOfStudent);
     }
 }
